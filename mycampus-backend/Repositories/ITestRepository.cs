9e634a6d4ce7e750f5022ab04b2d795c64be59da
Semineytor4 <semyilma@outlook.de>
2025-04-09 12:35:33 +0200

    Initial backend setup with test controller

---
+++ mycampus-backend/Repositories/ITestRepository.cs
@@ -0,0 +1,20 @@
+﻿using mycampus_backend.Models;
+
+namespace mycampus_backend.Repositories
+{
+    public interface ITestRepository
+    {
+        IEnumerable<TestModel> GetAll();
+    }
+
+    public class TestRepository : ITestRepository
+    {
+        public IEnumerable<TestModel> GetAll()
+        {
+            return new List<TestModel>
+            {
+                new TestModel { Id = 1, Message = "Hello World!" }
+            };
+        }
+    }
+}
