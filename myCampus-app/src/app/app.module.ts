42d25c464b33c6b88d21db35c80f89a74765d370
Lukas P <lukasp1209@gmail.com>
2025-04-11 12:29:58 +0200

    MC-76  Implement login with JWT token, error handling and create structure in project

---
+++ myCampus-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './features/auth/login/login.component';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { UserService } from './core/services/user.service';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    JwtInterceptor,
+    AppRoutingModule,
+    RouterModule.forRoot([]),
+  ],
+  providers: [
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ myCampus-app/src/app/core/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../../../src/environments/environment';
+import { UserLoginRequest } from '../models/user-login-request.model';
+import { UserLoginResponse } from '../models/user-login-response.model';
+import { jwtDecode } from 'jwt-decode';
+import { tap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = environment.apiUrl;
+  private currentUser = new BehaviorSubject<UserLoginResponse | null>(null);
+
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+  login(data: UserLoginRequest): Observable<UserLoginResponse> {
+    return this.http
+      .post<UserLoginResponse>(`${this.apiUrl}/auth/login`, data)
+      .pipe(
+        tap((response) => {
+          const decodedToken: any = jwtDecode(response.token);
+          const role = decodedToken.role;
+
+          this.cookieService.set('role', role, { expires: 1 });
+
+          this.cookieService.set('token', response.token, { expires: 1 });
+
+          this.currentUser.next(response);
+        })
+      );
+  }
+
+  getRole(): string | null {
+    return this.cookieService.get('role');
+  }
+
+  logout(): void {
+    this.currentUser.next(null);
+    this.cookieService.delete('token');
+    this.cookieService.delete('role');
+  }
+
+  get isAuthenticated(): boolean {
+    return !!this.cookieService.get('token');
+  }
+
+  get user$(): Observable<UserLoginResponse | null> {
+    return this.currentUser.asObservable();
+  }
+
+  getCurrentUser(): UserLoginResponse | null {
+    return this.currentUser.value;
+  }
+}
