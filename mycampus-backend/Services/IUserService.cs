08217a47b1bd5eb0811775c09cfb937489c0514a
Damjan Mlinaric <dami.mlinaric@gmail.com>
2025-04-12 13:23:21 +0200

    MC-35 removed old code fully

--- mycampus-backend/Services/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using mycampus_backend.Models;
-
-namespace mycampus_backend.Services
-{
-    public interface IUserService
-    {
-        Task<ApiResponse<object>> RegisterUser(UserRegistrationDto dto);
-    }
-}
