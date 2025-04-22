2b573cfebf29a26ace0147ec9cb3c805cdfd589d
Damjan Mlinaric <dami.mlinaric@gmail.com>
2025-04-22 22:14:14 +0200

    MC-41 selected students are ignored

--- my-campus-core/src/main/java/com/example/my_campus_core/controllers/api/user/v1/UserControllerREST.java
+++ my-campus-core/src/main/java/com/example/my_campus_core/controllers/api/user/v1/UserControllerREST.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.my_campus_core.service.UserService;
+import com.example.my_campus_core.util.Parsers;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/user/v1")
 public class UserControllerREST {
     private UserService userService;
+    private Parsers parsers;
 
-    public UserControllerREST(UserService userService) {
+    public UserControllerREST(UserService userService, Parsers parsers) {
         this.userService = userService;
+        this.parsers = parsers;
+
     }
 
     @GetMapping("/search/professors")
@@ -29,10 +33,10 @@ public class UserControllerREST {
     }
 
     @GetMapping("/search/students")
-    public ResponseEntity getStudents(@RequestParam String search, @RequestParam(defaultValue = "0") int ignoreId) {
-        List<Integer> ignoreIds = new ArrayList<>();
-        ignoreIds.add(ignoreId);
-        return new ResponseEntity(userService.getUsersByNameAndRole(search, "ROLE_STUDENT", ignoreIds), null, 200);
+    public ResponseEntity getStudents(@RequestParam String search, @RequestParam(defaultValue = "0") String ignoreIds) {
+        List<Integer> ignoreIdsList = parsers.parseIgnoreIds(ignoreIds);
+
+        return new ResponseEntity(userService.getUsersByNameAndRole(search, "ROLE_STUDENT", ignoreIdsList), null, 200);
     }
 
     @GetMapping("/info")
